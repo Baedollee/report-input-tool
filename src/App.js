d545f3700869427ef30a3d588564aa8e1d849c3a
baedollee <baesee0401@gmail.com>
2023-02-07 11:53:51 +0900

    절대경로 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Main from '../src/pages/reportInputPage/ReportInput';
-import CodeReportPage from '../src/pages/codeReportPage/CodeReportPage';
-import Test from './pages/test/Test';
+import Main from 'pages/reportInputPage/ReportInput';
+import CodeReportPage from 'pages/codeReportPage/CodeReportPage';
+import Test from 'pages/test/Test';
 
 function App() {
   return (
