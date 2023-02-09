e1894f78453c5940dadf7b6f92bb9b83870e3542
baedollee <baesee0401@gmail.com>
2023-02-09 18:08:16 +0900

    2/9

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import TestPage from 'pages/testPage/TestPage';
 import EntryPage from './pages/entryPage/EntryPage';
 import PlayerListPage from 'pages/playerListPage/PlayerListPage';
 import BootstrapTableTest from 'pages/testPage/bootStrapTable/BootstrapTableTest';
+import HitMapTestPage from 'pages/hitmapTestPage/HitMapTestPage';
 
 function App() {
   return (
@@ -14,10 +15,16 @@ function App() {
         <Route path='/' element={<Main />} />
         <Route path='/codeReport' element={<CodeReportPage />} />
       </Routes> */}
-      {/* <TestPage /> */}
       {/* <EntryPage /> */}
-      <PlayerListPage />
-      {/* <BootstrapTableTest /> */}
+
+      {/* 공부페이지 */}
+      {/* <TestPage /> */}
+
+      {/* 라인업 선수 클릭 이동 페이지 */}
+      {/* <PlayerListPage /> */}
+
+      {/* 히트맵 테스트 페이지 */}
+      <HitMapTestPage />
     </>
   );
 }
