d8af00811c3e10f1a09dffc4ca980dc1f031d00b
baedollee <baesee0401@gmail.com>
2023-02-13 18:37:51 +0900

    2/13

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 // import Main from 'pages/ReportInputPage/ReportInput';
 // import CodeReportPage from 'pages/CodeReportPage/CodeReportPage';
 // import TestPage from 'pages/TestPage/TestPage';
-import PlayerListPage from 'pages/PlayerListPage/PlayerListPage';
+import PlayerListPage from 'pages/playerListPage/PlayerListPage';
 // import BootstrapTableTest from 'pages/TestPage/bootStrapTable/BootstrapTableTest';
 // import HitMapTestPage from 'pages/HitmapTestPage/HitMapTestPage';
 // import LoginPage from 'pages/LoginPage/LoginPage';
